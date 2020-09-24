b3ff81e70441c53e903f64e6c97b2b9b514d7dd0
phz <penghuaizhen@163.com>
2020-09-24 21:44:00 +0800

    update time performance tools

--- dl_tutorial/spantime.py
+++ dl_tutorial/spantime.py
@@ -12,14 +12,16 @@ import matplotlib.patches as mpatches
 spans = []
 net = network.Network([784, 30, 10])
 
-for i in range(1, 10):
+for i in range(0, 10):
     t = net.SGD(training_data, 1, 10, 3.0, test_data=test_data)
     spans.append(t)
 
-print(spans, np.mean(spans))
+avg_t = np.mean(spans)
+
+print(spans, avg_t)
 
 # draw a picture
-red_patch = mpatches.Patch(color='red', label='Average {0}'.format(np.mean(spans)))
+red_patch = mpatches.Patch(color='red', label='Average {0}'.format(avg_t))
 plt.plot(spans)
 plt.xlabel('# of running')
 plt.ylabel('time span')
